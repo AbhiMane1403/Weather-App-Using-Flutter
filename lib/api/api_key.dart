1211a002bf41faa1ae7e6417fa30c19c56cd0bca
AbhiMane1403 <82831308+AbhiMane1403@users.noreply.github.com>
2024-02-08 03:21:52 +0530

    Add files via upload

---
+++ lib/api/api_key.dart
@@ -0,0 +1,1 @@
+const apiKey = "fddbc71cdb6b83ddfb1d2cdde70a0735";
---
+++ lib/api/fetch_weather.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: unused_import
+
+import 'dart:convert';
+
+import 'package:weatherapp_starter_project/api/api_key.dart';
+import 'package:weatherapp_starter_project/model/weather/weather_data.dart';
+import 'package:http/http.dart' as http;
+import 'package:weatherapp_starter_project/model/weather/weather_data_current.dart';
+import 'package:weatherapp_starter_project/model/weather/weather_data_daily.dart';
+import 'package:weatherapp_starter_project/model/weather/weather_data_hourly.dart';
+import 'package:weatherapp_starter_project/utils/api_url.dart';
+
+class FetchWeatherAPI {
+  WeatherData? weatherData;
+
+  Future<WeatherData> processData(lat, lon) async {
+    var response = await http.get(Uri.parse(apiURL(lat, lon)));
+    var jsonString = jsonDecode(response.body);
+    weatherData = WeatherData(
+        WeatherDataCurrent.fromJson(jsonString),
+        WeatherDataHourly.fromJson(jsonString),
+        WeatherDataDaily.fromJson(jsonString));
+
+    return weatherData!;
+  }
+}
